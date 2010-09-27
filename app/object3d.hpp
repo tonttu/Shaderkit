76bb30b8ef21ccce875a24b2bc1f0230f4e3115b
Riku Palomäki <riku@palomaki.fi>
2010-09-27 22:33:09 +0300

    More license tweaking

--- app/object3d.hpp
+++ app/object3d.hpp
@@ -3,9 +3,8 @@
  * This file is part of GLSL Lab.
  *
  * GLSL Lab is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU General Public License Version 3 as
+ * published by the Free Software Foundation.
  *
  * GLSL Lab is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
